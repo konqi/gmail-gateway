58b27b2c60d603ade932ced73ed065d0ae65a195
konqi <elkonqi@gmail.com>
2015-01-28 18:20:06 +0100

    - it's a start

---
+++ src/main/java/de/konqi/remailer/MailServlet.java
@@ -0,0 +1,32 @@
+package de.konqi.remailer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author konqi
+ */
+public class MailServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(MailServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        try {
+            MimeMessage message = new MimeMessage(session, req.getInputStream());
+        } catch (MessagingException e) {
+            logger.error("Could not extract message from request.", e);
+        }
+    }
+}
---
+++ src/main/java/de/konqi/remailer/Messaging.java
@@ -0,0 +1,92 @@
+package de.konqi.remailer;
+
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Base64;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author konqi
+ */
+public class Messaging {
+    /**
+     * Send an email from the user's mailbox to its recipient.
+     *
+     * @param service Authorized Gmail API instance.
+     * @param userId  User's email address. The special value "me"
+     *                can be used to indicate the authenticated user.
+     * @param email   Email to be sent.
+     * @throws MessagingException
+     * @throws IOException
+     */
+    private static void sendMessage(Gmail service, String userId, MimeMessage email)
+            throws MessagingException, IOException {
+        Message message = createMessageWithEmail(email);
+        message = service.users().messages().send(userId, message).execute();
+
+        System.out.println("Message id: " + message.getId());
+        System.out.println(message.toPrettyString());
+    }
+
+    /**
+     * Create a Message from an email
+     *
+     * @param email Email to be set to raw of message
+     * @return Message containing base64 encoded email.
+     * @throws IOException
+     * @throws MessagingException
+     */
+    private static Message createMessageWithEmail(MimeMessage email)
+            throws MessagingException, IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        email.writeTo(baos);
+        String encodedEmail = Base64.encodeBase64URLSafeString(baos.toByteArray());
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
+
+    /**
+     * @param from
+     * @param to
+     */
+    public static void sendMessage(String from, String to) {
+        AppIdentityCredential credential =
+                new AppIdentityCredential(Arrays.asList(GmailScopes.GMAIL_COMPOSE));
+        new Gmail.Builder(new UrlFetchTransport(), new JacksonFactory(), credential).build();
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+        try {
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(javax.mail.Message.RecipientType.TO,
+                    new InternetAddress(to));
+
+            message.setSubject("");
+            message.setText("");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
